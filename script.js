449beb60717802cd1b228bd07311b88ef3159579
Ganesh2048 <129187187+Ganesh2048@users.noreply.github.com>
2023-12-26 21:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+		var c=0;
+		for (let j = 0; j < str.length; j++) {
+			if(str.charAt(i)===str.charAt(j))
+				c++;
+		}
+		if(c===1)
+			return str.charAt(i);
+		
+	}
+	return None;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
